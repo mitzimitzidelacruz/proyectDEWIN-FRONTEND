79c34508f9e20f4f57f8263f9f6984586d2a3a38
vero <veronica.delacruz12@hotmail.com>
2022-12-05 00:13:27 -0600

    ADD: add changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyA4eG5EWsH9eCIuZ76I7ezQkzUZs6WH28M",
+    authDomain: "detallemaestro.firebaseapp.com",
+    projectId: "detallemaestro",
+    storageBucket: "detallemaestro.appspot.com",
+    messagingSenderId: "479302739489",
+    appId: "1:479302739489:web:2035a0ea9060378bb2a560",
+    measurementId: "G-RQ9SGZBQVL"
+  }
 };
